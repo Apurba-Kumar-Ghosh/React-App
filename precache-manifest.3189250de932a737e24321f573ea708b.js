9b6302a5a05cb3920251f2bda74ae8952b1822c6
Apurba Kr. Ghosh <apurba.kumarghosh.ece22@heritageit.edu.in>
2020-09-05 16:16:38 +0530

    Updates

---
+++ precache-manifest.3189250de932a737e24321f573ea708b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ca66c55574ee29c9bc8c26e5fc7794",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "cac25605a5a7b952cbfa",
+    "url": "/React-App/static/css/main.9c772113.chunk.css"
+  },
+  {
+    "revision": "2d2f226059dc69ebaad4",
+    "url": "/React-App/static/js/2.bd55b221.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.bd55b221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac25605a5a7b952cbfa",
+    "url": "/React-App/static/js/main.a0f75cc8.chunk.js"
+  },
+  {
+    "revision": "3f3625c1dd0a11c1a296",
+    "url": "/React-App/static/js/runtime-main.e5b3b9c8.js"
+  }
+]);
